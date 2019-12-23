448d119606a2d9833a828db52d08480ff70a3e78
Santosh Wadghule <santosh.wadghule@gmail.com>
2019-12-23 12:17:05 +0530

    Update user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,47 @@
-class UsersController < ApplicationController
+# frozen_string_literal: true
 
+class UsersController < ApplicationController
   def show
-    @user = User.includes(:email_addresses, :addresses).find_by_name("John Smith")
+    @user = User.includes(:email_addresses, :addresses)
+                .find_by_name('John Smith')
   end
 
   def edit
+    @user = User.includes(:email_addresses, :addresses).find(params[:id])
   end
 
   def update
+    email_address_index = params[:user][:primary_email_address]
+    params[:user][:email_addresses_attributes][email_address_index][:primary] = true
+
+    address_index = params[:user][:primary_address]
+    params[:user][:addresses_attributes][address_index][:default] = true
+
+    @user = User.find(params[:id])
+    respond_to do |format|
+      if @user.update(user_params)
+        flash[:notice] = 'User was successfully updated.'
+        format.html { redirect_to(@user) }
+      else
+        format.html { render action: 'edit' }
+      end
+    end
   end
-  
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      email_addresses_attributes: %i[
+        id
+        email
+        primary
+      ],
+      addresses_attributes: %i[
+        id
+        data
+        default
+      ]
+    )
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   has_many :email_addresses
   has_many :addresses, as: :owner
+
+  accepts_nested_attributes_for :email_addresses, :addresses
 end
